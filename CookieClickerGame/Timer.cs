7a8c3e189cf3304fb24eb99482dab566c02b3c8f
Domício Medeiros <domicioam@gmail.com>
2021-05-23 21:44:06 +0100

    Handling start message in Timer

--- CookieClickerGame/Timer.cs
+++ CookieClickerGame/Timer.cs
@@ -5,20 +5,66 @@ using System.Text;
 
 namespace CookieClickerGame
 {
-    class Timer : ReceiveActor
+    public class Timer : ReceiveActor
     {
         #region Messages
-        public class Start { }
+        public class Start
+        {
+            public Start(TimeSpan interval)
+            {
+                Interval = interval;
+            }
+
+            public TimeSpan Interval { get; }
+        }
+        public class Stop { }
         public class Tick { }
         #endregion
+
+        private ICancelable cancelable;
+
         public Timer()
         {
             Receive<Start>(HandleStart);
         }
 
-        private void HandleStart(Start obj)
+        private void Started()
         {
+            Receive<Stop>(HandleStop);
+            Receive<Tick>(HandleTick);
+        }
+
+        private void HandleStart(Start msg)
+        {
+            cancelable = Context
+                            .System
+                            .Scheduler
+                            .ScheduleTellRepeatedlyCancelable(
+                                msg.Interval,
+                                msg.Interval,
+                                Self,
+                                new Tick(),
+                                ActorRefs.NoSender);
+
+            BecomeStacked(Started);
+        }
+
+        private void HandleTick(Tick msg)
+        {
+            // publish tick to all listeners
             throw new NotImplementedException();
+        }
+
+        private void HandleStop(Stop obj)
+        {
+            cancelable?.Cancel();
+            UnbecomeStacked();
+        }
+
+        protected override void PostStop()
+        {
+            base.PostStop();
+            cancelable?.Cancel();
         }
     }
 }
---
+++ CookieClickerGameTests/TimerTests.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using Akka.TestKit.Xunit2;
+using CookieClickerGame;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CookieClickerGameTests
+{
+    public class TimerTests : TestKit
+    {
+        [Fact]
+        public void Should_start_timer()
+        {
+            var timer = Sys.ActorOf(Props.Create(() => new Timer()), "timer");
+            var start = new Timer.Start(TimeSpan.FromSeconds(5));
+
+        }
+    }
+}
