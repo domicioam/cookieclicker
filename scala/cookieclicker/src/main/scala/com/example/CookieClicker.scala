06d8c66c60c7207e96620b26da9f7c93611b44cf
Domício Medeiros <domicioam@gmail.com>
2021-10-03 14:19:32 +0100

    Cookieclicker scala

---
+++ scala/cookieclicker/src/main/scala/com/example/CookieClicker.scala
@@ -0,0 +1,20 @@
+import akka.actor.{Actor, ActorRef}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import akka.actor.Cancellable
+import akka.actor.ActorLogging
+import cookieclicker.Timer._
+import cookieclicker.Cookie
+import cookieclicker.Cookie.Click
+
+class CookieClicker(timer: ActorRef, cookie: ActorRef) extends Actor with ActorLogging  {
+  override def receive: Receive = {
+      case Tick => 
+          log.info("Tick received. Sending click to cookie.")
+          cookie ! Click
+  }
+  
+  override def preStart(): Unit = {
+      timer ! Subscribe
+  }
+}
---
+++ scala/cookieclicker/src/test/scala/com/example/CookieClickerSpec.scala
@@ -0,0 +1,4 @@
+
+class CookieClickerSpec {
+  // write integration test between cookie clicker and timer and score etc using event filters to practice
+}
