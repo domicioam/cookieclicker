4d16061557645c16626d87b8113ac32166980645
Domício Medeiros <domicioam@gmail.com>
2021-10-03 08:02:40 +0100

    ...

--- csharp/CookieClickerGame/Score.cs
+++ csharp/CookieClickerGame/Score.cs
@@ -15,30 +15,20 @@ namespace CookieClickerGame
 
         public Score()
         {
-            Receive<Increase>(HandleIncrease);
-            Receive<Decrease>(HandleDecrease);
-            Receive<GetScore>(HandleGetScore);
-        }
-
-        private void HandleGetScore(GetScore obj)
-        {
-            Sender.Tell(new Result<int>(score));
-        }
-
-        private void HandleDecrease(Decrease obj)
-        {
-            if(obj.Amount <= score)
+            Receive<Increase>(_ => score++);
+            Receive<Decrease>(obj =>
             {
-                score -= obj.Amount;
-                Sender.Tell(new Result<bool>(true));
-            }
-
-            Sender.Tell(new Result<bool>(false));
-        }
-
-        private void HandleIncrease(Increase increase)
-        {
-            score++;
+                if (obj.Amount <= score)
+                {
+                    score -= obj.Amount;
+                    Sender.Tell(new Result<bool>(true));
+                }
+                else
+                {
+                    Sender.Tell(new Result<bool>(false));
+                }
+            });
+            Receive<GetScore>(_ => Sender.Tell(new Result<int>(score)));
         }
     }
 }
